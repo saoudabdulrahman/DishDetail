e6ad1270dd5b2f15b07f1496ab102f09c3c9f427
Francis Medin Nicolas <179318168+fmnnicolas@users.noreply.github.com>
2026-02-13 22:43:09 +0800

    Wrap App component with AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<BrowserRouter>
-			<ScrollToTop />
-			<App />
+			<AuthProvider>
+				<App />
+			</AuthProvider>
 		</BrowserRouter>
 	</StrictMode>,
 );
