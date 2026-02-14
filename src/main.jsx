367114bf3311ee38ffb52cf98c6f60f8bfe0f60a
Saoud Abdulrahman <saoud_abdulrahman@dlsu.edu.ph>
2026-02-14 14:27:32 +0800

    Reorganize imports and include ScrollToTop component in main rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './auth/AuthContext.jsx';
+import ScrollToTop from './components/ScrollToTop';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<BrowserRouter>
 			<AuthProvider>
+				<ScrollToTop />
 				<App />
 			</AuthProvider>
 		</BrowserRouter>
