938e1db90dbfce90083cd7a52770f0009a089668
Saoud Abdulrahman <saoud_abdulrahman@dlsu.edu.ph>
2026-02-17 14:26:54 +0800

    feat: refactor routing structure and implement new routes for application navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
-import { Routes, Route } from 'react-router';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import ReviewsPage from './pages/ReviewsPage';
-import EstablishmentsPage from './pages/EstablishmentsPage';
-import EstablishmentPage from './pages/EstablishmentPage';
-import SubmitReview from './pages/SubmitReview';
-import SelectRestaurant from './pages/SelectRestaurant';
-import ProfilePage from './pages/ProfilePage';
-import NotFoundPage from './pages/NotFoundPage';
-import MainLayout from './components/MainLayout';
-import GuestRoute from './components/GuestRoute';
-import ProtectedRoute from './components/ProtectedRoute';
-
+import { Outlet, ScrollRestoration } from 'react-router';
 import './App.css';
 
 function App() {
 	return (
 		<div className="app-container">
-			<Routes>
-				<Route element={<GuestRoute />}>
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/signup" element={<SignupPage />} />
-				</Route>
-
-				<Route element={<MainLayout />}>
-					<Route index element={<HomePage />} />
-					<Route path="/reviews" element={<ReviewsPage />} />
-					<Route path="/establishments" element={<EstablishmentsPage />} />
-					<Route path="/establishments/:id" element={<EstablishmentPage />} />
-
-					<Route element={<ProtectedRoute />}>
-						<Route path="/profile" element={<ProfilePage />} />
-						<Route path="/submit-review" element={<SubmitReview />} />
-						<Route path="/select-restaurant" element={<SelectRestaurant />} />
-					</Route>
-
-					<Route path="*" element={<NotFoundPage />} />
-				</Route>
-			</Routes>
+			<Outlet />
+			<ScrollRestoration />
 		</div>
 	);
 }
